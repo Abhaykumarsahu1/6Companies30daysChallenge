7eb14d509ffec52b3dd0a5c8a8570f33b061467b
Abhay Kumar Sahu <aks.abhaykumarsahu@gmail.com>
2024-01-09 23:11:14 +0530

    day 8

---
+++ findtheWinneroftheCircularGame.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+ int solve(int n,int k){
+     if(n==1){
+         return 0;
+     }
+    return  (solve(n-1,k) +k)%n;
+ }
+    int findTheWinner(int n, int k) {
+        int ans= solve(n,k) +1;
+        return ans;
+    }
+};
+int main(){
+    Solution s;
+   cout<<"the winner is :"<< s.findTheWinner(5,2);
+    return 0;
+}
